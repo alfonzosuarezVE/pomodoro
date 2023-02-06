edc7b4bfae094cc7b85aaf2ba16d041aa6ebd635
samsuarr <personal.samuelsuarez@gmail.com>
2023-02-06 00:50:24 -0400

    I aadded a few variables

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const playButton = document.getElementById('play-button');
 
+
+let inputLearningMinutes = document.getElementById('learningMinutes');
+let inputRestingMinutes = document.getElementById('restingMinutes');
+let inputReflexionMinutes = document.getElementById('reflexionMinutes');
